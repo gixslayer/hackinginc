c70843537f2b74d229ec8125c95fcd7f8dd03ff1
Ciske H <ciske_harsema_2@hotmail.com>
2018-02-26 19:45:01 +0100

    Added sorting

--- assignment2/sws-assignment2-s1010048-s1009995/exercise1.c
+++ assignment2/sws-assignment2-s1010048-s1009995/exercise1.c
@@ -1,4 +1,10 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
+
+int cmp(const void* a, const void* b) {
+    return *(uintptr_t*)a - *(uintptr_t*)b;
+}
 
 void print_var(void* var, size_t size, const char* name) {
     unsigned char* ptr = var;
@@ -14,20 +20,26 @@ int main(int argc, char** argv) {
     long sn1 = 1010048;
     long sn2 = 1009995;
     int y[2] = {0x11223344, 0x44332211};
+    void* vars[] = {&i, &x, &y, &sn1, &sn2};
 
     printf("size of x:   %zu\n", sizeof(x));
     printf("size of i:   %zu\n", sizeof(i));
+    printf("size of y:   %zu\n", sizeof(y));
     printf("size of sn1: %zu\n", sizeof(sn1));
-    printf("size of sn2: %zu\n", sizeof(sn2));
-    printf("size of y:   %zu\n\n", sizeof(y));
+    printf("size of sn2: %zu\n\n", sizeof(sn2));
+
+    qsort(vars, 5, sizeof(void*), cmp);
 
     printf("address        | content (hex) | content (dec) | variable\n");
     printf("---------------|---------------|---------------|---------\n");
-    print_var(&i, sizeof(i), "i");
-    print_var(&x, sizeof(x), "x");
-    print_var(&sn1, sizeof(sn1), "sn1");
-    print_var(&sn2, sizeof(sn2), "sn2");
-    print_var(&y, sizeof(y), "y");
+
+    for(int j = 0; j < 5; ++j) {
+        if(vars[j] == &i) print_var(&i, sizeof(i), "i");
+        if(vars[j] == &x) print_var(&x, sizeof(x), "x");
+        if(vars[j] == &y) print_var(&y, sizeof(y), "y");
+        if(vars[j] == &sn1) print_var(&sn1, sizeof(sn1), "sn1");
+        if(vars[j] == &sn2) print_var(&sn2, sizeof(sn2), "sn2");
+    }
 
     return 0;
 }
